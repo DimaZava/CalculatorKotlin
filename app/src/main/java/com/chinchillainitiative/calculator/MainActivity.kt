68502bbe5c61be80d6782fecb6c04805af1e4791
Dmitry Zavadsky <dimazava@hotmail.com>
2024-10-05 15:05:06 +0300

    Added animation to collapse/expanding cells

--- app/src/main/java/com/chinchillainitiative/calculator/MainActivity.kt
+++ app/src/main/java/com/chinchillainitiative/calculator/MainActivity.kt
@@ -3,11 +3,16 @@ package com.chinchillainitiative.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -16,10 +21,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.chinchillainitiative.calculator.ui.theme.CalculatorTheme
 
@@ -39,7 +45,7 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MyApp(names: List<String>, modifier: Modifier = Modifier) {
-        var shouldShowOnboarding by remember { mutableStateOf(true) }
+        var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
         Surface(
             modifier
         ) {
@@ -50,7 +56,7 @@ class MainActivity : ComponentActivity() {
             }
             else {
                 Greetings(
-                    names = names,
+                    // names = names,
                     modifier = Modifier.fillMaxSize()
                 )
             }
@@ -79,20 +85,34 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun Greetings(names: List<String>, modifier: Modifier = Modifier) {
-        Column(
+    fun Greetings(
+        modifier: Modifier = Modifier,
+        names: List<String> = List(1000) { "$it" }
+    ) {
+        LazyColumn(
             modifier = modifier.padding(vertical = 4.dp)
         ) {
-            for (name in names) {
+            items(items = names) {name ->
                 Greeting(name)
             }
+//            for (name in names) {
+//                Greeting(name)
+//            }
         }
     }
 
     @Composable
     fun Greeting(name: String, modifier: Modifier = Modifier) {
-        val expanded = remember { mutableStateOf(false) }
-        val extraPadding = if (expanded.value) 48.dp else 0.dp
+        var expanded by rememberSaveable { mutableStateOf(false) }
+        val extraPadding by animateDpAsState(
+            if (expanded) 48.dp else 0.dp,
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            ),
+            label = ""
+        )
+
         Surface(
             color = MaterialTheme.colorScheme.primary,
             modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -103,15 +123,18 @@ class MainActivity : ComponentActivity() {
                 Column(
                     modifier = Modifier
                         .weight(1f)
-                        .padding(bottom = extraPadding)
+                        .padding(
+                            bottom = extraPadding
+                                .coerceAtLeast(0.dp)
+                        )
                 ) {
                     Text("Hello")
                     Text(name)
                 }
                 ElevatedButton(
-                    onClick = { expanded.value = !expanded.value }
+                    onClick = { expanded = !expanded }
                 ) {
-                    Text(if (expanded.value) "Show less" else "Show more")
+                    Text(if (expanded) "Show less" else "Show more")
                 }
             }
         }
